30e39c3e9eef6b58925aae368e507f3db3b0bea9
Simon J Stuart <lakraven@gmail.com>
2026-09-03 20:56:56 +0200

    Expose Radio peer registry

--- src/ESPressio_Radio.hpp
+++ src/ESPressio_Radio.hpp
@@ -3,6 +3,7 @@
 #include "ESPressio_RadioTypes.hpp"
 #include "ESPressio_RadioObservers.hpp"
 #include "ESPressio_IRadio.hpp"
+#include "ESPressio_RadioPeerRegistry.hpp"
 #include "ESPressio_RadioTransport.hpp"
 #include "ESPressio_RadioWorker.hpp"
 #include "ESPressio_RadioClockSynchronizer.hpp"
