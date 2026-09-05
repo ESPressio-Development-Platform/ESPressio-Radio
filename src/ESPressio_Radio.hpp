09ab3f0eaa7d8169f68491176fc2dd1943bd150d
Simon J Stuart <lakraven@gmail.com>
2026-09-05 09:48:01 +0200

    Expose deferred logical transfer observer bridge

--- src/ESPressio_Radio.hpp
+++ src/ESPressio_Radio.hpp
@@ -5,6 +5,7 @@
 #include "ESPressio_IRadio.hpp"
 #include "ESPressio_RadioPeerRegistry.hpp"
 #include "ESPressio_DeferredLogicalTransferTracker.hpp"
+#include "ESPressio_DeferredLogicalTransferObserverBridge.hpp"
 #include "ESPressio_RadioTransport.hpp"
 #include "ESPressio_RadioWorker.hpp"
 #include "ESPressio_RadioClockSynchronizer.hpp"
